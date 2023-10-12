61bb7d68d6411acff7a2c36ec76b9435f6f3cf05
RodrigoAhumadaE <rod.ahumada@duocuc.cl>
2023-10-12 17:47:16 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Crear una lista vacía de tipo objeto
+using System.ComponentModel;
+
+List<object> objetos = new List<object>();
+
+// Agregue los siguientes valores a la lista: 7, 28, -1, true, "chair"
+objetos.Add(7);
+objetos.Add(28);
+objetos.Add(-1);
+objetos.Add(true);
+objetos.Add("chair");
+
+// Recorra la lista e imprima todos los valores.
+foreach(object valor in objetos){
+  Console.WriteLine(valor);
+}
+
+// Sume todos los valores que son de tipo Int y genere la suma
+int sum = 0;
+foreach(object valor in objetos){
+  if(valor is int){
+    sum += (int)valor;
+  }
+}
+Console.WriteLine($"La suma de los valores de tipo Int es {sum}");
